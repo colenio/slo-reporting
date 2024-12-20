d3ba563b73a879f1d1a47a4b29145fca64819e75
Marcel Körtgen <marcel.koertgen@colenio.com>
2024-12-20 19:20:49 +0100

    test: finish slo-export integration test

--- src/tests/slo/test_api.py
+++ src/tests/slo/test_api.py
@@ -1,14 +1,18 @@
-import json
-
-import pytest
+from pandas import DataFrame
+from pytest_mock import MockFixture
 from starlette.testclient import TestClient
 
 from config.settings import settings
+from slo.service import get_slo_service
+from tests.utils import patch_of
 
+def test_slo_export(client: TestClient, mocker: MockFixture) -> None:
+    p = patch_of(mocker, get_slo_service().get_slo_window)  # type: ignore
+    df = DataFrame()
+    p.return_value = df
 
-def test_slo_export(client: TestClient) -> None:
     path = f"{settings.api_base}/slo/export"
-    response = client.get(f"{path}/csv")
+    response = client.get(f"{path}/csv?update=true")
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "text/csv; charset=utf-8"
     assert response.headers["Content-Disposition"] == 'attachment; filename="slo-reporting.csv"'
